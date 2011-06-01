74a333459febf183588be9955065f151310a5a75
erik <cordsen@gmail.com>
2011-05-31 23:15:37 -0400

    added css and title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
 	render_views
 
   before(:each) do
-  	@base_title = "Ruby on Rails Tutorial Sample App | "
+  	@base_title = "Ruby on Rails Tutorial Sample App"
   end
   
   describe "GET 'home'" do
@@ -14,7 +14,7 @@ describe PagesController do
     end
 		it "should have a title" do
 			get 'home'
-			response.should have_selector("title",:content => @base_title+"Home")
+			response.should have_selector("title",:content => @base_title + " | Home")
 		end
   end
 
@@ -25,7 +25,7 @@ describe PagesController do
     end
 		it "should have a title" do
 			get 'contact'
-			response.should have_selector("title",:content => @base_title+"Contact")
+			response.should have_selector("title",:content => @base_title + " | Contact")
 		end
   end
 
@@ -36,7 +36,7 @@ describe PagesController do
     end
 		it "should have a title" do
 			get 'about'
-			response.should have_selector("title",:content => @base_title+"About")
+			response.should have_selector("title",:content => @base_title + " | About")
 		end
   end
 
@@ -47,7 +47,7 @@ describe PagesController do
     end
 		it "should have a title" do
 			get 'help'
-			response.should have_selector("title",:content => @base_title+"Help")
+			response.should have_selector("title",:content => @base_title + " | Help")
 		end
   end
 end
